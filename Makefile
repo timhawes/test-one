2c91477728e52aa6fa942afe5570f1606e2c8085
Tim Hawes <me@timhawes.com>
2018-10-24 23:53:38 +0100

    Test case

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 hello: hello.c
 	$(CC) -o hello hello.c
 
+test: hello
+	./test | grep "Hello"
+
 clean:
 	rm hello
--- hello.c
+++ hello.c
@@ -2,5 +2,5 @@
 
 int main(int argc, char const *argv[])
 {
-    printf("Hello World\n");
+    printf("Hi World\n");
 }
